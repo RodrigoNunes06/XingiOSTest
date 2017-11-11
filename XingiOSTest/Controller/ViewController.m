42788e7b0737835869ae65844dbb5a1f0e93a795
Rodrigo Nunes <rodrigo.nunes06@gmail.com>
2017-11-11 09:22:37 +0100

    Finish collectionView implementation, add shadows and radius.Add pagination

--- XingiOSTest/Controller/ViewController.m
+++ XingiOSTest/Controller/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 #import "GitHubRepository.h"
 #import "CollectionViewCell.h"
+#import "GitHubRemoteService.h"
 
 @interface ViewController () <UICollectionViewDataSource, UICollectionViewDelegateFlowLayout>
 
@@ -25,6 +26,7 @@
     self.view = [[UIView alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.repositoryArray = [NSMutableArray new];
     [self setUpCollectionView];
+    [self loadDataWithParameter:nil];
 }
 
 - (void)setUpCollectionView {
@@ -44,10 +46,53 @@
     [self.view addSubview:self.collectionView];
 }
 
+- (void)loadDataWithParameter:(NSDictionary *__nullable)aParameters {
+    GitHubRemoteService *dataSource = [GitHubRemoteService new];
+    
+    [dataSource getRepositoriesForUser:@"xing" parameters:aParameters onSuccess:^(NSArray<GitHubRepository *> * _Nullable repositoryArray) {
+        [self.repositoryArray addObjectsFromArray:repositoryArray];
+        [self.collectionView reloadData];
+    } onError:^(NSError * _Nullable error) {
+        [self showAlertViewControllerWithErrorMessage:error];
+    }];
+}
+
+- (void)showAlertViewControllerWithErrorMessage:(NSError *)errorMessage {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Opps!" message:[NSString stringWithFormat:@"%@",errorMessage] preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *closeAction = [UIAlertAction actionWithTitle:@"Close" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        [self dismissViewControllerAnimated:YES completion:nil];
+    }];
+    [alert addAction:closeAction];
+    [self showViewController:alert sender:self];
+}
+
+- (void)setCellBackgroundColor:(CollectionViewCell *)aCell withFork:(BOOL)aFork {
+    if (aFork) {
+        aCell.backgroundColor = [UIColor colorWithRed:76/255.0 green:217/255.0 blue:100/255.0 alpha:1.0];
+    } else {
+        aCell.backgroundColor = [UIColor whiteColor];
+    }
+}
+
 #pragma mark - CollectionView Datasource
 
 - (nonnull __kindof UICollectionViewCell *)collectionView:(nonnull UICollectionView *)collectionView cellForItemAtIndexPath:(nonnull NSIndexPath *)indexPath {
     CollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:[CollectionViewCell getCellReuseIdentifier] forIndexPath:indexPath];
+    
+    GitHubRepository *repositoryObject = [self.repositoryArray objectAtIndex:indexPath.item];
+    
+    if (repositoryObject.repoDescription != nil) {
+        cell.descriptionLabel.text = repositoryObject.repoDescription;
+    }
+    if (repositoryObject.repoName != nil) {
+        cell.nameLabel.text = repositoryObject.repoName;
+    }
+    if (repositoryObject.ownerLogin != nil) {
+        cell.ownerLoginLabel.text = repositoryObject.ownerLogin;
+    }
+    
+    [self setCellBackgroundColor:cell withFork:repositoryObject.fork];
+    
     return cell;
 }
 
@@ -55,7 +100,15 @@
     return [self.repositoryArray count];
 }
 
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
+    return CGSizeMake(self.view.frame.size.width/1.5, self.view.frame.size.height/3);
+}
 
+- (void)collectionView:(UICollectionView *)collectionView willDisplayCell:(UICollectionViewCell *)cell forItemAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.item + 1 == [self.repositoryArray count]) {
+        [self loadDataWithParameter:@{@"page": @([self.repositoryArray count]/30 + 1)}];
+    }
+}
 
 
 @end
--- XingiOSTest/View/CollectionViewCell.m
+++ XingiOSTest/View/CollectionViewCell.m
@@ -14,10 +14,42 @@
     self = [super initWithFrame:frame];
     
     if (self) {
-        
+        [self setText];
+        [self setStyle];
     }
     
     return self;
+}
+
+- (void)setText {
+    self.nameLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 20, self.frame.size.width, 20)];
+    self.descriptionLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 100, self.frame.size.width, 100)];
+    self.ownerLoginLabel = [[UILabel alloc] initWithFrame: CGRectMake(0, 60, self.frame.size.width, 20)];
+    
+    [self.nameLabel setTextAlignment:NSTextAlignmentCenter];
+    [self.nameLabel setNumberOfLines:0];
+    [self.descriptionLabel setTextAlignment:NSTextAlignmentCenter];
+    [self.descriptionLabel setNumberOfLines:0];
+    [self.ownerLoginLabel setTextAlignment:NSTextAlignmentCenter];
+    
+    [self addSubview:self.nameLabel];
+    [self addSubview:self.descriptionLabel];
+    [self addSubview:self.ownerLoginLabel];
+}
+
+- (void)setStyle {
+    // border radius
+    [self.layer setCornerRadius:30.0f];
+    
+    // border
+    [self.layer setBorderColor:[UIColor lightGrayColor].CGColor];
+    [self.layer setBorderWidth:1.5f];
+    
+    // drop shadow
+    [self.layer setShadowColor:[UIColor blackColor].CGColor];
+    [self.layer setShadowOpacity:0.8];
+    [self.layer setShadowRadius:3.0];
+    [self.layer setShadowOffset:CGSizeMake(2.0, 2.0)];
 }
 
 + (NSString * __nonnull)getCellReuseIdentifier {
