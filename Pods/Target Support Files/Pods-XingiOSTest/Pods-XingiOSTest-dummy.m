3c425a0820f75938f7050fd308f74531d3afb40c
Rodrigo Nunes <rodrigo.nunes06@gmail.com>
2017-11-11 07:36:40 +0100

    Add AFNetworking Pod

---
+++ Pods/Target Support Files/Pods-XingiOSTest/Pods-XingiOSTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_XingiOSTest : NSObject
+@end
+@implementation PodsDummy_Pods_XingiOSTest
+@end
---
+++ Pods/Target Support Files/Pods-XingiOSTestTests/Pods-XingiOSTestTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_XingiOSTestTests : NSObject
+@end
+@implementation PodsDummy_Pods_XingiOSTestTests
+@end
