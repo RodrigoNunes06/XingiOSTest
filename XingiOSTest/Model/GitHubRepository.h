5dc9fa441ef68ebd5266989011192eb2d3a778f1
Rodrigo Nunes <rodrigo.nunes06@gmail.com>
2017-11-11 07:39:42 +0100

    create GitHubRepository model

---
+++ XingiOSTest/Model/GitHubRepository.h
@@ -0,0 +1,13 @@
+//
+//  GitHubRepository.h
+//  XingiOSTest
+//
+//  Created by Rodrigo Nunes on 11/11/17.
+//  Copyright © 2017 Rodrigo Nunes Gil. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GitHubRepository : NSObject
+
+@end
---
+++ XingiOSTest/Model/GitHubRepository.m
@@ -0,0 +1,13 @@
+//
+//  GitHubRepository.m
+//  XingiOSTest
+//
+//  Created by Rodrigo Nunes on 11/11/17.
+//  Copyright © 2017 Rodrigo Nunes Gil. All rights reserved.
+//
+
+#import "GitHubRepository.h"
+
+@implementation GitHubRepository
+
+@end
