f1e4d24d876f34870010dd19c6c16dc78d282012
Rodrigo Nunes <rodrigo.nunes06@gmail.com>
2017-11-11 08:40:41 +0100

    set up collection view programmatically

--- XingiOSTest/Controller/ViewController.m
+++ XingiOSTest/Controller/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import "GitHubRepository.h"
 
-@interface ViewController ()
+@interface ViewController () <UICollectionViewDataSource, UICollectionViewDelegateFlowLayout>
+
+@property (strong, nonatomic, nonnull) UICollectionView *collectionView;
+@property (strong, nonatomic, nullable) NSMutableArray<GitHubRepository *> *repositoryArray;
 
 @end
 
@@ -16,7 +20,31 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view = [[UIView alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.repositoryArray = [NSMutableArray new];
+    [self setUpCollectionView];
 }
+
+- (void)setUpCollectionView {
+    UICollectionViewFlowLayout *layout=[[UICollectionViewFlowLayout alloc] init];
+    self.collectionView=[[UICollectionView alloc] initWithFrame:self.view.frame collectionViewLayout:layout];
+    [self.collectionView setDataSource:self];
+    [self.collectionView setDelegate:self];
+    
+    [layout setMinimumLineSpacing:10];
+    [layout setMinimumInteritemSpacing:10];
+    [layout setSectionInset:UIEdgeInsetsMake(0, 0, 0, 0)];
+    [layout setScrollDirection:UICollectionViewScrollDirectionVertical];
+    
+    [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"cellIdentifier"];
+    [self.collectionView setBackgroundColor:[UIColor lightGrayColor]];
+    
+    [self.view addSubview:self.collectionView];
+}
+
+
+
+
 
 @end
